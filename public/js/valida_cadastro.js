f452846a26b53c48183359068e745df25b1087dc
Arthur Aguiar Estigarribia <arthuraguiarestigarribia@hotmail.com>
2017-10-30 14:24:34 -0200

    Recomeço do projeto com correção de bugs.

---
+++ public/js/valida_cadastro.js
@@ -0,0 +1,55 @@
+function validaEmail() {
+    var email = document.getElementById('email').value;
+
+    if (email != 'admin') {
+        if (!email.includes('@')) {
+            return false;
+        }
+        
+        var provedor = '';
+
+        for (var i = 0; i < email.length; i++) {
+            if (email[i] == '@') {
+                provedor = email.substring(i++, email.length);
+                break;
+            }
+        }
+
+        if (!provedor.includes('.')) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+
+function validaSenha() {
+    var senha = document.getElementById('senha').value;
+
+    if (senha == '') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validaNome() {
+    var nome = document.getElementById('nome').value;
+
+    if (nome == '') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+document.getElementById('form').onsubmit = function(){
+	if (validaEmail() && validaSenha() && validaNome()) {
+        document.getElementById('form').submit();
+    } else {
+    //    alert('Email inválido');
+        document.getElementById('erro').innerHTML = 'Email inválido.';
+    }
+};
